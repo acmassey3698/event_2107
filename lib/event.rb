61a6e056b52b98d7ad59faa7d231407ad94a49b1
Andrew Massey <acmassey@ncsu.edu>
2021-08-02 16:58:58 -0400

    Complete all but overstocked items in iteration 3

--- lib/event.rb
+++ lib/event.rb
@@ -24,4 +24,39 @@ class Event
       truck.inventory.include?(item)
     end
   end
+
+  def all_items
+    @food_trucks.flat_map do |truck|
+      truck.inventory.keys
+    end.uniq
+  end
+
+  def sorted_item_list
+    ordered_items =
+    all_items.sort_by do |item|
+      item.name
+    end
+
+    ordered_items.map do |item|
+      item.name
+    end
+  end
+
+  def total_quantity(item)
+    @food_trucks.sum do |truck|
+      truck.check_stock(item)
+    end
+  end
+
+  def total_inventory
+    event_inventory = Hash.new(0)
+    all_items.each do |item|
+      event_inventory[item] = {quantity: total_quantity(item), food_trucks: food_trucks_that_sell(item)}
+    end
+    event_inventory
+  end
+
+  def overstocked_items
+
+  end
 end
