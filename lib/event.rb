a21b23983c64227ce86667aba3994d3d7fd81ff9
Andrew Massey <acmassey@ncsu.edu>
2021-08-02 15:56:53 -0400

    Complete iteration 2

---
+++ lib/event.rb
@@ -0,0 +1,27 @@
+class Event
+  attr_reader :name,
+              :food_trucks
+
+  def initialize(name)
+    @name = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(truck)
+    @food_trucks << truck
+  end
+
+  def food_truck_names
+    names = []
+    @food_trucks.each do |truck|
+      names << truck.name
+    end
+    names
+  end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.find_all do |truck|
+      truck.inventory.include?(item)
+    end
+  end
+end
